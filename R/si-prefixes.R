205d7ab84ffd029615757c23e2774f4eddbfef83
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-01-01 21:55:09 +0200

    Add scale_x_wl_continuous(). Polish documentation of all new fucntions.

--- R/si-prefixes.R
+++ R/si-prefixes.R
@@ -1,28 +1,38 @@
-# internal
+# internal data table used by functions in this file
 #
 exp_pfix.df <-
-  tibble::tibble(exponents = c(-24, -21, -18, -15, -12, -9, -6, -3, -2, -1, 0,
-                           1, 2, 3, 6, 9, 12, 15, 18, 21, 24),
-             prefixes = c("y", "z", "a", "f", "p", "n", "u", "m", "c", "d", "",
-                          "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
-             prefixes.utf8 = c("y", "z", "a", "f", "p", "n", "\u03bc", "m", "c", "d", "",
-                          "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
-             prefixes.latex = c("y", "z", "a", "f", "p", "n", "$\\mu$", "m", "c", "d", "",
-                               "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
-             names = c("yopto", "zepto", "atto", "fempto",
-                       "pico", "nano", "micro", "milli", "centi", "deci", "",
-                       "deca", "hecto", "kilo", "mega", "giga", "tera",
-                       "peta", "exa", "zetta", "yotta"))
+  tibble::tibble(exponents =
+                   c(-24, -21, -18, -15, -12, -9, -6, -3, -2, -1, 0,
+                     1, 2, 3, 6, 9, 12, 15, 18, 21, 24),
+                 ascii =
+                   c("y", "z", "a", "f", "p", "n", "u", "m", "c", "d", "",
+                     "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
+                 utf8 =
+                   c("y", "z", "a", "f", "p", "n", "\u03bc", "m", "c", "d", "",
+                     "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
+                 latex =
+                   c("y", "z", "a", "f", "p", "n", "$\\mu$", "m", "c", "d", "",
+                     "da", "h", "k", "M", "G", "T", "P", "E", "Z", "Y"),
+                 names =
+                   c("yopto", "zepto", "atto", "fempto",
+                     "pico", "nano", "micro", "milli", "centi", "deci", "",
+                     "deca", "hecto", "kilo", "mega", "giga", "tera",
+                     "peta", "exa", "zetta", "yotta"))
 
 #' SI unit prefixes
 #'
-#' Convert SI unit prefixes to exponents and vice-versa.
+#' Convert SI unit prefixes into exponents of ten of multipliers and vice-versa.
 #'
 #' @param exponent numeric The power of 10 of the unit multiplier.
-#' @param encoding character How to encode Greek letters, "utf8", "ascii",
-#'   "LaTeX".
+#' @param char.set character How to encode Greek letters and other fancy
+#'    characters in prefixes: \code{"utf8"}, \code{"ascii"}, \code{"LaTeX"}.
 #'
 #' @export
+#'
+#' @note To change the default \code{char.set}, set R option
+#'   \code{"photobiology.fancy.chars"}. Implementation is based on a table of
+#'   data and extensible to any alphabet supported by R character objects by
+#'   expanding the table.
 #'
 #' @examples
 #'
@@ -31,14 +41,13 @@ exp_pfix.df <-
 #' exponent2prefix(-6)
 #'
 exponent2prefix <- function(exponent,
-                            encoding = "utf8") {
-  encoding <- tolower(encoding)
-  if (encoding == "utf8") {
-    prefixes <- "prefixes.utf8"
-  } else if (encoding == "latex") {
-    prefixes <- "prefixes.latex"
+                            char.set = getOption("photobiology.fancy.chars",
+                                                 default = "utf8")) {
+  char.set <- tolower(gsub("-", "", char.set))
+  if (char.set %in% names(exp_pfix.df)) {
+    prefixes <- char.set
   } else {
-    prefixes <- "prefixes"
+    prefixes <- "ascii"
   }
   if (!exponent %in% exp_pfix.df[["exponents"]]) {
     NA_character_
@@ -84,14 +93,13 @@ prefix_name2exponent <- function(name) {
 #' @export
 #'
 prefix2exponent <- function(prefix,
-                            encoding = "utf8") {
-  encoding <- tolower(encoding)
-  if (encoding == "utf8") {
-    prefixes <- "prefixes.utf8"
-  } else if (encoding == "LaTeX") {
-    prefixes <- "prefixes.latex"
+                            char.set = getOption("photobiology.fancy.chars",
+                                                 default = "utf8")) {
+  char.set <- tolower(gsub("-", "", char.set))
+  if (char.set %in% names(exp_pfix.df)) {
+    prefixes <- char.set
   } else {
-    prefixes <- "prefixes"
+    prefixes <- "ascii"
   }
   if (!prefix %in% exp_pfix.df[[prefixes]]) {
     NA_integer_
---
+++ R/wl-axis-utils.R
@@ -0,0 +1,235 @@
+#' Wavelength conversions
+#'
+#' Convert wavelength into wavenumber or into frequency.
+#'
+#' @param w.length numeric wavelength (nm)
+#' @param unit.exponent integer
+#'
+#' @export
+#'
+#' @examples
+#'
+#' w_number(600)
+#' w_frequency(600)
+#'
+w_number <- function(w.length, unit.exponent = 0) {
+  1e9 / w.length * 10^unit.exponent # 1/m
+}
+
+#' @rdname w_number
+#'
+#' @export
+#'
+w_frequency <- function(w.length, unit.exponent = 0) {
+  299792458 / (w.length * 1e-9) / 10^unit.exponent # speed of light [m/s] / w.length [m]
+}
+
+#' Wave- axis labels
+#'
+#' Generate wavelength, wavenumber and wave frequency axis labels in SI units,
+#' using SI scale factors. Output can be selected as character, expression (R
+#' default devices) or LaTeX (for tikz device).
+#'
+#' @param format character string, "R", "R.expresion", "R.character", or
+#'   "LaTeX".
+#' @param unit.exponent integer
+#'
+#' @return a character string or an R expression.
+#'
+#' @export
+#'
+#' @examples
+#' w_length_label()
+#' w_length_label("R.expression")
+#' w_length_label("LaTeX")
+#' w_number_label()
+#' w_number_label("R.expression")
+#' w_frequency_label()
+#' w_frequency_label("R.expression")
+#'
+w_length_label <- function(unit.exponent = -9,
+                           format = getOption("photobiology.math",
+                                              default = "R.expression")) {
+  if (tolower(format) == "latex") {
+    if (has_SI_prefix(unit.exponent)) {
+      paste("Wavelength (",
+            exponent2prefix(unit.exponent, char.set = "LaTeX"),
+            "m)", sep = "")
+    } else {
+      paste("Wavelength ($\\times 10^{",
+            unit.exponent,
+            "}$~m)", sep = "")
+    }
+  } else if (format %in% c("R.expression")) {
+    if (has_SI_prefix(unit.exponent)) {
+      prefix <- exponent2prefix(unit.exponent)
+      bquote(plain(Wavelength)~(plain(.(prefix))*plain(m)))
+    } else {
+      bquote(plain(Wavelength)~(10^{.(unit.exponent)}~plain(m)))
+    }
+  } else if (format == "R.character" &&
+             has_SI_prefix(unit.exponent)) {
+    paste("Wavelength (",
+          exponent2prefix(unit.exponent, char.set = "ascii"),
+          "m)", sep = "")
+  } else {
+    warning("'format = ", format,
+            "' not implemented for unit.exponent = ", unit.exponent)
+  }
+}
+
+#' @rdname w_length_label
+#'
+#' @export
+#'
+w_number_label <- function(unit.exponent = 0,
+                           format = getOption("photobiology.math",
+                                              default = "R.expression")) {
+  if (tolower(format) == "latex") {
+    if (has_SI_prefix(unit.exponent)) {
+      paste("Wavenumber (",
+            exponent2prefix(unit.exponent, char.set = "LaTeX"),
+            "m$^{-1}$)", sep = "")
+    } else {
+      paste("Wavenumber ($\\times 10^{",
+            unit.exponent,
+            "m$^{-1}$)", sep = "")
+    }
+  } else if (format %in% c("R.expression")) {
+    if (has_SI_prefix(unit.exponent)) {
+      prefix <- exponent2prefix(unit.exponent)
+      bquote(plain(Wavenumber)~(plain(.(prefix))*plain(m)^{-1}))
+    } else {
+      bquote(plain(Wavenumber)~(10^{.(unit.exponent)}~plain(m)^{-1}))
+    }
+  } else if (format == "R.character" &&
+             has_SI_prefix(unit.exponent)) {
+    paste("Wavenumber (1/",
+          exponent2prefix(unit.exponent, char.set = "ascii"),
+          "m)", sep = "")
+  } else {
+    warning("'format = ", format,
+            "' not implemented for unit.exponent = ", unit.exponent)
+    NA_character_
+  }
+}
+
+#' @rdname w_length_label
+#'
+#' @export
+#'
+w_frequency_label <- function(unit.exponent = 9,
+                              format = getOption("photobiology.math",
+                                                 default = "R.expression")) {
+  if (tolower(format) == "latex") {
+    if (has_SI_prefix(unit.exponent)) {
+      paste("Frequency (",
+            exponent2prefix(unit.exponent, char.set = "LaTeX"),
+            "Hz)", sep = "")
+    } else {
+      paste("Frequency ($\\times 10^{",
+            unit.exponent,
+            "}$~Hz)", sep = "")
+    }
+  } else if (format %in% c("R.expression")) {
+    if (has_SI_prefix(unit.exponent)) {
+      prefix <- exponent2prefix(unit.exponent)
+      bquote(plain(Frequency)~(plain(.(prefix))*plain(Hz)))
+    } else {
+      bquote(plain(Frequency)~(10^{.(unit.exponent)}~plain(Hz)))
+    }
+  } else if (format == "R.character" &&
+             has_SI_prefix(unit.exponent)) {
+    paste("Frequency (",
+          exponent2prefix(unit.exponent, char.set = "ascii"),
+          "Hz)", sep = "")
+  } else {
+    warning("'format = ", format,
+            "' not implemented for unit.exponent = ", unit.exponent)
+    NA_character_
+  }
+}
+
+#' Secondary axes for wavelengths
+#'
+#' Secondary axes for wavelength data in nanometres. With suitable scaling and
+#' name (axis label) for frequency and wavenumber.
+#'
+#' @param unit.exponent integer
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' library(photobiology)
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_continuous(name = w_length_label(),
+#'                      sec.axis = sec_axis_w_number())
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_continuous(name = w_length_label(),
+#'                      sec.axis = sec_axis_w_number(-4))
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_continuous(name = w_length_label(),
+#'                      sec.axis = sec_axis_w_number(nearest_SI_exponent(-4)))
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_continuous(name = w_length_label(),
+#'                      sec.axis = sec_axis_w_number(-3))
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_continuous(name = w_length_label(),
+#'                      sec.axis = sec_axis_w_frequency())
+#'
+sec_axis_w_number <- function(unit.exponent = -6) {
+  ggplot2::sec_axis(trans = ~w_number(., unit.exponent),
+                    name = w_number_label(unit.exponent),
+                    breaks = scales::pretty_breaks(n = 7))
+}
+
+#' @rdname sec_axis_w_number
+#'
+#' @export
+#'
+sec_axis_w_frequency <- function(unit.exponent = 12) {
+  ggplot2::sec_axis(trans = ~w_frequency(., unit.exponent),
+                    name = w_frequency_label(unit.exponent),
+                    breaks = scales::pretty_breaks(n = 7)
+  )
+}
+
+#' Wavelength x-scale
+#'
+#' Scale x continuous with defaults suitable for wavelengths in nanometres.
+#'
+#' @note This function only alters two default arguments, please, see
+#' documentation for \code{\link[ggplot2]{scale_x_continuous}}
+#'
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' library(photobiology)
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_wl_continuous()
+#'
+#' ggplot(sun.spct) +
+#'   geom_line() +
+#'   scale_x_wl_continuous(sec.axis = sec_axis_w_number())
+#'
+scale_x_wl_continuous <- function(name = w_length_label(),
+                                  breaks = scales::pretty_breaks(n=7),
+                                  ...) {
+  scale_x_continuous(name = name,
+                     breaks = breaks,
+                     ...)
+}
