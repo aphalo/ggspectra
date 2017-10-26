3b144c64c62983c1e4460cd16e310b98d8c804fd
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-10-26 12:49:51 +0300

    Add title modifiers "class", "inst.sn", and "inst.name". Update vignette.

--- R/title-utils.R
+++ R/title-utils.R
@@ -4,8 +4,10 @@
 #'
 #' @param x generic_spct The spectral object plotted.
 #' @param x.name character The name of the object being plotted.
-#' @param annotations character vector The annotations "sdirectives".
-#' @param ... named arguments pased to \code{ggtitle()}
+#' @param annotations character vector Annotations as described for
+#'   \code{plot()} methods, values unrelated to title are ignored.
+#' @param default.title character vector The default used for \code{annotations
+#'   = "title"}.
 #'
 #' @return The return value of \code{ggplot2::ggtitle()}.
 #'
@@ -13,31 +15,33 @@
 #'
 ggtitle_spct <- function(x,
                          x.name = NULL,
-                         annotations = "title:objt") {
+                         annotations = "title",
+                         default.title  = "title:objt") {
+
+  get_title_text <- function(key) {
+    switch(key,
+           objt = x.name,
+           class = class(x)[1],
+           what = getWhatMeasured(x)[[1]],
+           when = getWhenMeasured(x),
+           where = paste(getWhereMeasured(x), collapse = "; "),
+           inst.name = getInstrDesc(x)[["spectrometer.name"]],
+           inst.sn = getInstrDesc(x)[["spectrometer.sn"]],
+           none = NULL,
+           {warning("Title key '", key, "' not recognized"); NULL}
+    )
+  }
+
   title.ann <- grep("^title", annotations, value = TRUE)
   if (length(title.ann) == 0) {
     return(NULL)
   } else if(title.ann[1] == "title") {
     # default title
-    title.ann <- "title:objt"
+    title.ann <- default.title
   }
   title.ann <- c(strsplit(title.ann[1], ":")[[1]][-1], "none", "none")
-    title <-
-      switch(title.ann[1],
-             objt = x.name,
-             what = getWhatMeasured(x)[[1]],
-             when = getWhenMeasured(x),
-             where = paste(getWhereMeasured(x), collapse = "; "),
-             none = NULL,
-             NULL)
-    subtitle <-
-      switch(title.ann[2],
-             objt = x.name,
-             what = getWhatMeasured(x)[[1]],
-             when = getWhenMeasured(x),
-             where = paste(getWhereMeasured(x), collapse = "; "),
-             none = NULL,
-             NULL)
+  title <- get_title_text(title.ann[1])
+  subtitle <- get_title_text(title.ann[2])
   ggtitle(title, subtitle)
 }
 
