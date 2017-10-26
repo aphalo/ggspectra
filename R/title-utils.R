1763f6c96f8071c7d898833af181cd9b0b60a01c
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2017-10-26 17:18:27 +0300

    Add visual test cases for titles.

--- R/title-utils.R
+++ R/title-utils.R
@@ -1,6 +1,7 @@
 #' Add title to a spectral plot
 #'
-#' Add a title to a spectral plot based on metadata
+#' Add a title to a spectral plot based on metadata stored in an spectral
+#' object.
 #'
 #' @param x generic_spct The spectral object plotted.
 #' @param x.name character The name of the object being plotted.
@@ -9,12 +10,30 @@
 #' @param default.title character vector The default used for \code{annotations
 #'   = "title"}.
 #'
+#' @details \code{ggtitle_spct()} retrieves from object \code{x} metadata and
+#' passes it to \code{ggplot2::ggtitle()} as arguments for \code{title} and
+#' \code{subtitle}. The specification for the tittle is passed as argument
+#' to \code{annotations}, and consists in the keyword \code{title} with optional
+#' modifiers selecting the kind of metatdata to use, separated by colons.
+#' "objt", "class", "what", "when", "where", "inst.name", "inst.sn" and "none"
+#' are recognized as modifiers to "title".
+#'
 #' @return The return value of \code{ggplot2::ggtitle()}.
+#'
+#' @examples
+#' library(ggplot2)
+#' library(photobiology)
+#'
+#' p <- ggplot(sun.spct) +
+#'   geom_line()
+#'
+#' p + ggtitle_spct(sun.spct)
+#' p + ggtitle_spct(sun.spct, annotations = "title:where:when")
 #'
 #' @export
 #'
 ggtitle_spct <- function(x,
-                         x.name = NULL,
+                         x.name = deparse(substitute(x)),
                          annotations = "title",
                          default.title  = "title:objt") {
 
---
+++ tests/testthat/test-title.R
@@ -0,0 +1,57 @@
+context("ggtitle_spct")
+library(photobiology)
+
+test_that("title", {
+  vdiffr::expect_doppelganger("cps-title",
+                              plot(white_led.cps_spct, annotations = c("+", "title")))
+  vdiffr::expect_doppelganger("cps-title-objt",
+                              plot(white_led.cps_spct, annotations = c("+", "title:objt")))
+  vdiffr::expect_doppelganger("cps-title-class",
+                              plot(white_led.cps_spct, annotations = c("+", "title:class")))
+  vdiffr::expect_doppelganger("cps-title-what",
+                              plot(white_led.cps_spct, annotations = c("+", "title:what")))
+  vdiffr::expect_doppelganger("cps-title-when",
+                              plot(white_led.cps_spct, annotations = c("+", "title:when")))
+  vdiffr::expect_doppelganger("cps-title-where",
+                              plot(white_led.cps_spct, annotations = c("+", "title:where")))
+  vdiffr::expect_doppelganger("cps-title-inst.name",
+                              plot(white_led.cps_spct, annotations = c("+", "title:inst.name")))
+  vdiffr::expect_doppelganger("cps-title-inst.sn",
+                              plot(white_led.cps_spct, annotations = c("+", "title:inst.sn")))
+  vdiffr::expect_doppelganger("cps-title-none",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none")))
+})
+
+test_that("subtitle", {
+  vdiffr::expect_doppelganger("cps-title-none-objt",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:objt")))
+  vdiffr::expect_doppelganger("cps-title-none-class",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:class")))
+  vdiffr::expect_doppelganger("cps-title-none-what",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:what")))
+  vdiffr::expect_doppelganger("cps-title-none-when",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:when")))
+  vdiffr::expect_doppelganger("cps-title-none-where",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:where")))
+  vdiffr::expect_doppelganger("cps-title-none-inst.name",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:inst.name")))
+  vdiffr::expect_doppelganger("cps-title-none-inst.sn",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:inst.sn")))
+  vdiffr::expect_doppelganger("cps-title-none-none",
+                              plot(white_led.cps_spct, annotations = c("+", "title:none:none")))
+})
+
+test_that("ggtitle_spct", {
+  vdiffr::expect_doppelganger("cps-ggtitle-default",
+                              ggplot(white_led.cps_spct) +
+                                geom_line() +
+                                ggtitle_spct(white_led.cps_spct))
+  vdiffr::expect_doppelganger("cps-ggtitle-object",
+                              ggplot(white_led.cps_spct) +
+                                geom_line() +
+                                ggtitle_spct(white_led.cps_spct, annotations = "title:objt"))
+  vdiffr::expect_doppelganger("cps-ggtitle-object-class",
+                              ggplot(white_led.cps_spct) +
+                                geom_line() +
+                                ggtitle_spct(white_led.cps_spct, annotations = "title:objt:class"))
+})
