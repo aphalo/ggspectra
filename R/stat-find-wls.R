94c6d3c1b8a9140066b7ae95dbdbc011b0dc58b5
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2018-09-23 10:38:13 +0300

    Add support for vectors and lists as arguments accepted by parameter target of stat_find_wls() and stat_find_qty().

--- R/stat-find-wls.R
+++ R/stat-find-wls.R
@@ -1,34 +1,36 @@
 #' Find wavelength for target quantity value.
 #'
-#' \code{stat_find_wls} finds at which x positions values equal to a
-#' target are located.
+#' \code{stat_find_wls} finds at which x positions values equal to a target are
+#' located.
 #'
 #' @param mapping The aesthetic mapping, usually constructed with
-#'    \code{\link[ggplot2]{aes}} or \code{\link[ggplot2]{aes_}}. Only needs to be set
-#'    at the layer level if you are overriding the plot defaults.
+#'   \code{\link[ggplot2]{aes}} or \code{\link[ggplot2]{aes_}}. Only needs to be
+#'   set at the layer level if you are overriding the plot defaults.
 #' @param data A layer specific dataset - only needed if you want to override
-#'    the plot defaults.
+#'   the plot defaults.
 #' @param geom The geometric object to use display the data
-#' @param position The position adjustment to use for overlapping points
-#'    on this layer
+#' @param position The position adjustment to use for overlapping points on this
+#'   layer
 #' @param show.legend logical. Should this layer be included in the legends?
-#'   \code{NA}, the default, includes if any aesthetics are mapped.
-#'   \code{FALSE} never includes, and \code{TRUE} always includes.
-#' @param inherit.aes If \code{FALSE}, overrides the default aesthetics,
-#'   rather than combining with them. This is most useful for helper functions
-#'   that define both data and aesthetics and shouldn't inherit behaviour from
-#'   the default plot specification, e.g. \code{\link[ggplot2]{borders}}.
-#' @param ... other arguments passed on to \code{\link[ggplot2]{layer}}. This can
-#'   include aesthetics whose values you want to set, not map. See
+#'   \code{NA}, the default, includes if any aesthetics are mapped. \code{FALSE}
+#'   never includes, and \code{TRUE} always includes.
+#' @param inherit.aes If \code{FALSE}, overrides the default aesthetics, rather
+#'   than combining with them. This is most useful for helper functions that
+#'   define both data and aesthetics and shouldn't inherit behaviour from the
+#'   default plot specification, e.g. \code{\link[ggplot2]{borders}}.
+#' @param ... other arguments passed on to \code{\link[ggplot2]{layer}}. This
+#'   can include aesthetics whose values you want to set, not map. See
 #'   \code{\link[ggplot2]{layer}} for more details.
-#' @param na.rm	a logical value indicating whether NA values should be
-#'   stripped before the computation proceeds.
-#' @param target numeric value indicating the spectral quantity value for which
-#'   wavelengths are to be searched and interpolated if need. The character
-#'   strings "half.maximum" and "half.range" are also accepted as arguments.
-#' @param interpolate logical Indicating whether the nearest wavelength value
-#'   in \code{x} should be returned or a value calculated by linear
-#'   interpolation between wavelength values stradling the target.
+#' @param na.rm	a logical value indicating whether NA values should be stripped
+#'   before the computation proceeds.
+#' @param target numeric vector indicating the spectral quantity values for
+#'   which wavelengths are to be searched and interpolated if need. The
+#'   \code{character} strings "half.maximum" and "half.range" are also accepted
+#'   as arguments. A list with \code{numeric} and/or \code{character} values is
+#'   also accepted.
+#' @param interpolate logical Indicating whether the nearest wavelength value in
+#'   \code{x} should be returned or a value calculated by linear interpolation
+#'   between wavelength values stradling the target.
 #' @param label.fmt character  string giving a format definition for converting
 #'   values into character strings by means of function \code{\link{sprintf}}.
 #' @param x.label.fmt character  string giving a format definition for
@@ -41,31 +43,20 @@
 #' @return A data frame with one row for each match to \code{target} found in
 #'   the data.
 #'
-#' @section Computed variables:
-#' \describe{
-#'   \item{x}{x-value at or nearest to the match to the target as numeric}
-#'   \item{y}{target value or y-value nearest to the target as numeric}
-#'   \item{x.label}{x-value at or nearest to the match formatted as character}
-#'   \item{y.label}{target value or y-value nearest to the target formatted as character}
-#'   \item{color}{color definition calculated by assuming that x-values are
-#'   wavelengths expressed in nanometres.}
-#' }
+#' @section Computed variables: \describe{ \item{x}{x-value at or nearest to the
+#'   match to the target as numeric} \item{y}{target value or y-value nearest to
+#'   the target as numeric} \item{x.label}{x-value at or nearest to the match
+#'   formatted as character} \item{y.label}{target value or y-value nearest to
+#'   the target formatted as character} \item{color}{color definition calculated
+#'   by assuming that x-values are wavelengths expressed in nanometres.} }
 #'
-#' @section Default aesthetics:
-#' Set by the statistic and available to geoms.
-#' \describe{
-#'   \item{label}{..x.label..}
-#'   \item{xintercept}{..x..}
-#'   \item{yintercept}{..y..}
-#'   \item{fill}{..color..}
-#' }
+#' @section Default aesthetics: Set by the statistic and available to geoms.
+#'   \describe{ \item{label}{..x.label..} \item{xintercept}{..x..}
+#'   \item{yintercept}{..y..} \item{fill}{..color..} }
 #'
-#' @section Required aesthetics:
-#' Required by the statistic and need to be set with \code{aes()}.
-#' \describe{
-#'   \item{x}{numeric, wavelength in nanometres}
-#'   \item{y}{numeric, a spectral quantity}
-#' }
+#' @section Required aesthetics: Required by the statistic and need to be set
+#'   with \code{aes()}. \describe{ \item{x}{numeric, wavelength in nanometres}
+#'   \item{y}{numeric, a spectral quantity} }
 #'
 #' @seealso \code{\link[photobiology]{find_peaks}}.
 #'
@@ -76,22 +67,21 @@
 #'   \code{geom_hline} and \code{geom_vline}. The formatting of the labels
 #'   returned can be controlled by the user.
 #'
-#' @note These stats work nicely together with geoms
-#'   \code{geom_text_repel} and
-#'   \code{geom_label_repel} from package
-#'   \code{\link[ggrepel]{ggrepel}} to solve the problem of overlapping labels
-#'   by displacing them. To discard overlapping labels use \code{check_overlap =
-#'   TRUE} as argument to \code{geom_text}.
-#'  By default the labels are character values suitable to be plotted as is, but
-#'  with a suitable \code{label.fmt} labels suitable for parsing by the geoms
-#'  (e.g. into expressions containing greek letters or super or subscripts) can
-#'  be also easily obtained.
+#' @note These stats work nicely together with geoms \code{geom_text_repel} and
+#'   \code{geom_label_repel} from package \code{\link[ggrepel]{ggrepel}} to
+#'   solve the problem of overlapping labels by displacing them. To discard
+#'   overlapping labels use \code{check_overlap = TRUE} as argument to
+#'   \code{geom_text}. By default the labels are character values suitable to be
+#'   plotted as is, but with a suitable \code{label.fmt} labels suitable for
+#'   parsing by the geoms (e.g. into expressions containing greek letters or
+#'   super or subscripts) can be also easily obtained.
 #'
 #' @examples
 #' library(photobiology)
 #' library(ggplot2)
 #' # ggplot() methods for spectral objects set a default mapping for x and y.
-#' ggplot(yellow_gel.spct) + geom_line() + stat_find_wls(target = 0.75)
+#' ggplot(yellow_gel.spct) + geom_line() +
+#'   stat_find_wls(target = c(0.25, 0.5, 0.75))
 #' ggplot(yellow_gel.spct) + geom_line() +
 #'   stat_find_wls(target = "half.maximum", geom = "point", colour = "red") +
 #'   stat_find_wls(target = "half.maximum", geom = "text", colour = "red",
@@ -145,12 +135,18 @@ StatFindWls <-
                                             label.fmt,
                                             x.label.fmt,
                                             y.label.fmt) {
-                     wls.df <- photobiology::find_wls(data,
-                                                      .fun = `<=`,
-                                                      target,
-                                                      interpolate,
-                                                      col.name.x = "x",
-                                                      col.name = "y")
+                     hits.ls <- list()
+                     for (t in target) {
+                       hits.ls <-
+                         c(hits.ls,
+                           list(photobiology::find_wls(data,
+                                                       .fun = `<=`,
+                                                       target = t,
+                                                       interpolate = interpolate,
+                                                       col.name.x = "x",
+                                                       col.name = "y")))
+                     }
+                     wls.df <- dplyr::bind_rows(hits.ls)
                      dplyr::mutate(wls.df,
                                    x.label = sprintf(x.label.fmt, x),
                                    y.label = sprintf(y.label.fmt, y),
@@ -190,9 +186,11 @@ StatFindWls <-
 #'   \code{\link[ggplot2]{layer}} for more details.
 #' @param na.rm	a logical value indicating whether NA values should be stripped
 #'   before the computation proceeds.
-#' @param target numeric value indicating the spectral quantity value for which
-#'   wavelengths are to be searched and interpolated if need. The character
-#'   strings "half.maximum" and "half.range" are also accepted as arguments.
+#' @param target numeric vector indicating the spectral quantity values for
+#'   which wavelengths are to be searched and interpolated if need. The
+#'   \code{character} strings "half.maximum" and "half.range" are also accepted
+#'   as arguments. A list with \code{numeric} and/or \code{character} values is
+#'   also accepted.
 #' @param interpolate logical Indicating whether the nearest wavelength value in
 #'   \code{x} should be returned or a value calculated by linear interpolation
 #'   between wavelength values stradling the target.
@@ -246,7 +244,10 @@ StatFindWls <-
 #' library(photobiology)
 #' library(ggplot2)
 #' # ggplot() methods for spectral objects set a default mapping for x and y.
-#' ggplot(yellow_gel.spct) + geom_line() + stat_find_qty(target = "half.range")
+#' ggplot(yellow_gel.spct) + geom_line() +
+#'   stat_find_qty(target = "half.range")
+#' ggplot(yellow_gel.spct) + geom_line() +
+#'   stat_find_qty(target = c(490, 500, 510))
 #' ggplot(yellow_gel.spct) + geom_line() +
 #'   stat_find_qty(target = 500, geom = "point", colour = "red") +
 #'   stat_find_qty(target = 500, geom = "text", colour = "red",
@@ -301,13 +302,18 @@ StatFindQty <-
                                             x.label.fmt,
                                             y.label.fmt) {
                      # By swapping the column names, we obtain qty values
-                     # This is a bit of computational overkill for spectra
-                     qty.df <- photobiology::find_wls(data,
-                                                      .fun = `<=`,
-                                                      target,
-                                                      interpolate,
-                                                      col.name.x = "y",
-                                                      col.name = "x")
+                     hits.ls <- list()
+                     for (t in target) {
+                       hits.ls <-
+                         c(hits.ls,
+                           list(photobiology::find_wls(data,
+                                                       .fun = `<=`,
+                                                       target = t,
+                                                       interpolate,
+                                                       col.name.x = "y",
+                                                       col.name = "x")))
+                     }
+                     wls.df <- dplyr::bind_rows(hits.ls)
                      dplyr::mutate(qty.df,
                                    x.label = sprintf(x.label.fmt, x),
                                    y.label = sprintf(y.label.fmt, y),
